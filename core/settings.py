2d0f2e5aba92a1eb46e3225db9a2042c0828b62c
Kapeesh Kaul <kkapeesh1999@gmail.com>
2025-03-27 05:15:14 -0600

    add settings to handle env imports

---
+++ core/settings.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+class Settings:
+    def __init__(self):
+        # Load environment variables from .env file
+        load_dotenv(override=True)
+        
+        # Configuration for Neo4j
+        self.NEO4J_URI = os.getenv("NEO4J_URI")
+        self.NEO4J_USER = os.getenv("NEO4J_USERNAME")
+        self.NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
+
+# Create a singleton instance of Settings
+settings = Settings() 
